052ddb6756a02a7aa43a334560f851e73a42d0e0
tabib-arham <140184089+tabib-arham@users.noreply.github.com>
2026-01-18 15:39:32 +0600

    ss

---
+++ beecrowd 1003 simple sum.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,SOMA;// 3 box a,b ,soma
+    scanf("%d%d",&A,&B);// a=5,b=5
+    SOMA= A+B;//soma=5+5, soma=10
+    printf("SOMA = %d\n",SOMA);// soma = %d
+}
