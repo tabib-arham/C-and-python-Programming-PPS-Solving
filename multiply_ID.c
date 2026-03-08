d52cf6eb7b192d5d91f77d39efcdb29440057d68
tabib-arham <140184089+tabib-arham@users.noreply.github.com>
2026-03-09 00:16:04 +0600

    a

--- multiply_ID.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    long long int number;
-    long long int id=25335045;
-    scanf("%lld",&number);
-    long long int multiply = id*number;
-    printf("result = %lld\n",multiply);
-}
